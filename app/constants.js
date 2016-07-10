6eb4b83048956a51f7e9aa9297525aaaf71f9f7e
Alex Aralis <alex.aralis@gmail.com>
2016-07-10 00:28:19 +0000

    moved the localStorage stuff into a meganote.Cacher class that can be injected and used as a coupling.

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,5 @@
 {
-  angular.module('meganote')
-    .constant('API_BASE', 'http://localhost:3030/api/v1/');
+    angular.module('meganote')
+        .constant('API_BASE', 'http://localhost:3030/api/v1/');
 }
+
